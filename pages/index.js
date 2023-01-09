597ee6355d8c7b37ab1ba46bb62d9468480ea096
Hamayoon Ashraf <hashraf@ryerson.ca>
2023-01-08 22:49:50 -0500

    Add index page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,46 @@
 import Image from "next/image";
+import Navbar from "../components/navbar/Navbar";
+import AboutSection from "../components/section/AboutSection";
+import ResumeSection from "../components/section/ResumeSection";
+import SpaceFiller from "../components/space-fillers/SpaceFiller";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-black text-white grid place-content-center">
-      <div className="relative">
-        <span className="leading-[160px] lg:text-[220px] text-indigo-500 fade font-extrabold absolute top-60 tracking-widest -left-[255px]">HAMAYOON</span>
-        <div className="relative fade-image">
-          <Image
-            src="/hamayoon-visionx.png"
-            alt="Hamayoon VisionX"
-            width={1000}
-            height={1000}
-          />
+    <div>
+      <Navbar />
+      {/* <div className="h-screen bg-white text-black dark:bg-black dark:text-white grid place-content-center overflow-hidden select-none">
+        <div className="relative bottom-5">
+          <span className="leading-[160px] block tracking-[0.2em] -mr-[0.2em] lg:tracking-widest text-[10vw] xl:text-[220px] text-indigo-500 fade font-extrabold relative">
+            HAMAYOON
+          </span>
+          <div className="absolute fade-image xl:-top-[240px] xl:left-64 z-50 inline-block -top-4 left-8">
+            <Image
+              src="/hamayoon-visionx.png"
+              alt="Hamayoon VisionX"
+              width={1000}
+              height={1000}
+              quality={100}
+              priority={true}
+            />
+          </div>
         </div>
+      </div> */}
+      <div className="h-screen bg-white text-black dark:bg-black dark:text-white flex items-center overflow-hidden select-none">
+        <div className="relative bottom-16 xl:bottom-24 w-full">
+          <div className="leading-[10vw] block absolute w-full text-[13vw] -mr-[0.1em] tracking-[0.1em] text-center text-indigo-500 fade font-extrabold">
+            HAMAYOON
+          </div>
+          <div className="absolute fade-image -bottom-[26vw] left-[28%] z-50">
+            <img src="/hamayoon-visionx2.png" className="w-[45vw]" />
+          </div>
+        </div>
+      </div>
+      <div className="dark:bg-black">
+        <SpaceFiller />
+        <AboutSection />
+        <SpaceFiller />
+        <ResumeSection />
+        <SpaceFiller />
       </div>
     </div>
   );
